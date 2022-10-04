bcc8cfa71d6d600a3ff5de910268528fbdf0293b
Antoine3012 <77623896+Antoine3012@users.noreply.github.com>
2022-10-04 12:56:09 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  ngOnInit() {}
+  ngOnInit() {
+    const interval$ = interval(1000);
+
+    setTimeout(() => interval$.subscribe((value) => console.log(value)), 3000);
+  }
 }
